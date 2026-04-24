62df26542ad1eef6570b0477995f916e0affb948
Carlos Martinez <141792555+CSGLMZBA@users.noreply.github.com>
2026-04-24 11:32:09 -0600

    [UPDATED]: port

--- backend/app/config/app.php
+++ backend/app/config/app.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'app_name' => 'PHP-FIREBASE',
-    'base_url' => 'http://localhost/',
+    'base_url' => 'http://localhost:8888/',
     'jwt_secret' => 'secret',
     'jwt_exp_minutes' => 480,
     'timezone' => 'America/Mexico_city'
